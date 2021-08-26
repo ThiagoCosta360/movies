2a11eee14e4723420854d30f4cabd5df656b7c60
thiago costa <tcaa@icomp.ufam.edu.br>
2021-08-25 21:30:51 -0400

    feat: user dropdown

--- src/app/layouts/components/user-dropdown/user-dropdown.component.ts
+++ src/app/layouts/components/user-dropdown/user-dropdown.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NbMenuItem, NbMenuService } from '@nebular/theme';
+import { filter, map } from 'rxjs/operators';
+
 
 @Component({
 	selector: 'app-user-dropdown',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./user-dropdown.component.scss']
 })
 export class UserDropdownComponent implements OnInit {
+	
+	items: NbMenuItem[] = [
+		{ title: 'Profile',
+			icon: 'person-outline'
+		},
+		{ title: 'Logout',
+			icon: 'log-out-outline'
+		},
+	];
 
-	constructor() { }
-
-	ngOnInit(): void {
+	constructor(
+	private nbMenuService: NbMenuService, 
+	) {
 	}
-
+	
+	ngOnInit(): void {
+		this.nbMenuService.onItemClick()
+			.pipe(
+				filter(({ tag }) => tag === 'my-context-menu'),
+				map(({ item: { title } }) => title),
+			)
+			.subscribe(title => console.log(`${title} was clicked!`));
+	}
 }
+	
--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideMenuLayoutComponent } from './side-menu-layout/side-menu-layout.component';
 import { CardLayoutComponent } from './card-layout/card-layout.component';
-import { NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule } from '@nebular/theme';
+import { NbContextMenuModule, NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbUserModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HeaderComponent } from './components/header/header.component';
 import { ThemeChangerComponent } from './components/theme-changer/theme-changer.component';
@@ -25,6 +25,8 @@ import { UserDropdownComponent } from './components/user-dropdown/user-dropdown.
 		NbSidebarModule,
 		NbIconModule,
 		NbEvaIconsModule,
+		NbUserModule,
+		NbContextMenuModule,
 	],
 	exports: [
 		SideMenuLayoutComponent,
